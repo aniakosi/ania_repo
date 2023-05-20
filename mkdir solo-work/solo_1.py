20353c875ee33ef9f4929aad37f0b312e358c05a
Anna Kosinska <akosinska36@gmail.com>
2023-05-20 19:05:32 +0200

    zadanie 1.2

--- mkdir solo-work/solo_1.py
+++ mkdir solo-work/solo_1.py
@@ -7,3 +7,11 @@ student = "Ola"
 # wykorzystaj w princie zmienne hello i student
 
 print("{} {}".format(hello, student))
+
+
+# zadanie 1.2
+studentname = input("Wpisz swoje imie")
+
+print("Hello Ola")
+
+print("Hello {}".format(studentname))
