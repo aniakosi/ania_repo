a4821b81e8a6789ca8da693339869bb80ea11ee2
Anna Kosinska <akosinska36@gmail.com>
2023-05-20 19:09:08 +0200

    zadanie 1.3

--- mkdir solo-work/solo_1.py
+++ mkdir solo-work/solo_1.py
@@ -15,3 +15,12 @@ studentname = input("Wpisz swoje imie")
 print("Hello Ola")
 
 print("Hello {}".format(studentname))
+
+# zadanie 1.3
+
+studenci = ["Ania", "Kuba", "Piotr", "Jan"]
+
+# policz liczbe studentow w tablicy studenci 
+# oczekiwany rezultat: Liczba studentow wynosi: 4
+liczba_studentow = len(studenci)
+print("Liczba studentow wynosi: {}".format(liczba_studentow))
